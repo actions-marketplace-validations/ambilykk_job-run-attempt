eb6cbd6a2f73ff5747bd394307b358cf3a9e65c2
Ambily <ambilykk@Ambilys-MacBook-Pro.local>
2023-01-04 15:33:00 +0530

    Added debug message

--- app/index.js
+++ app/index.js
@@ -10,34 +10,40 @@ const octokit = github.getOctokit(token);
 
 // use the current run id
 const run_id = github.context.runId;
-const currentJob= github.job;
+const currentJob = github.job;
 
 // define the run function
 async function run() {
     // define output array
     let run_attempt = 1;
 
+    // console log the start of the run
+    console.log("Starting run ");
+
     // List jobs for the workflow run to capture the current job id
-    const jobsdata=await octokit.rest.actions.listJobsForWorkflowRun({
+    const jobsdata = await octokit.rest.actions.listJobsForWorkflowRun({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
         run_id: run_id
-      })
+    })
 
-      for (const jobinfo of jobsdata.data.jobs) {
-       if(jobinfo.name == currentJob){
-          
-          // get the current job details including run attempt
-          const job_data=await octokit.rest.actions.getJobForWorkflowRun({
-            owner: github.context.repo.owner,
-            repo: github.context.repo.repo,
-            job_id: jobinfo.id
-          })
+    console.log("Current Job: " + currentJob);
 
-          // Get Job attempt and return as output/result
-          console.log("Run Attempt: "+ job_data.data.run_attempt)
-          run_attempt = job_data.data.run_attempt;
-        }  
+    for (const jobinfo of jobsdata.data.jobs) {
+        if (jobinfo.name == currentJob) {
+
+            console.log("Job Identified");
+            // get the current job details including run attempt
+            const job_data = await octokit.rest.actions.getJobForWorkflowRun({
+                owner: github.context.repo.owner,
+                repo: github.context.repo.repo,
+                job_id: jobinfo.id
+            })
+
+            // Get Job attempt and return as output/result
+            console.log("Run Attempt: " + job_data.data.run_attempt)
+            run_attempt = job_data.data.run_attempt;
+        }
     }
 
     // set the output value
